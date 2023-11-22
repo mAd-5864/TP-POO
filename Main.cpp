12d882f4194365a650c804aa65b3b63158708c53
Pedro Amorim <pepas52@gmail.com>
2023-11-22 16:53:19 +0000

    Revert "Revert "update command verifications""
    This reverts commit fc66085a10d3b2612092060b64d1957ab6b4f45d.

--- Main.cpp
+++ Main.cpp
@@ -26,28 +26,34 @@ void getUserCommand() {
                 cout << "[ERRO] Sintaxe invalida: avanca <n>" << endl;
             }
         } else if (command == "hnova") {
-            if (arguments.size()!=3){
+            if (arguments.size()==3){
                 int nLines;
                 int nCols;
                 istringstream (arguments[1])>>nLines;
                 istringstream (arguments[2])>>nCols;
                 //criarHab(nLines, nCols);
+            } else {
+                cout << "[ERRO] Sintaxe invalida: hnova <num linhas> <num colunas>" << endl;
             }
         } else if (command == "hrem") {
             //removerHab();
         } else if (command == "znova") {
-            if (arguments.size()!=3){
+            if (arguments.size()==3){
                 int X;
                 int Y;
                 istringstream (arguments[1])>>X;
                 istringstream (arguments[2])>>Y;
                 //criarHab(nLines, nCols);
+            }else{
+                cout << "[ERRO] Sintaxe invalida: znova <linha> <coluna>" << endl;
             }
         } else if (command == "zrem") {
             if (arguments.size()>1){
                 int idZona;
                 istringstream (arguments[1])>>idZona;
                 //removerZona(idZona);
+            }else{
+                cout << "[ERRO] Sintaxe invalida: hnova <ID zona>" << endl;
             }
         } else if (command == "zlista") {
             //mostrarZonas();
@@ -55,13 +61,41 @@ void getUserCommand() {
             if (arguments.size()>1){
                 int idZona;
                 istringstream (arguments[1])>>idZona;
-                //listarComponentes(idZona);
+                //listarComponentesZona(idZona);
+            }else{
+                cout << "[ERRO] Sintaxe invalida: zcomp <ID zona>" << endl;
             }
         } else if (command == "zprops") {
             if (arguments.size()>1){
                 int idZona;
                 istringstream (arguments[1])>>idZona;
-                //removerZona(idZona);
+                //listarPropriedadesZona(idZona);
+            }else{
+                cout << "[ERRO] Sintaxe invalida: zprops <ID zona>" << endl;
+            }
+        } else if (command == "pmod") {
+            if (arguments.size()==4){
+                int idZona;
+                string nomeProp;
+                int valorProp;
+                istringstream (arguments[1])>>idZona;
+                istringstream (arguments[2])>>nomeProp;
+                istringstream (arguments[3])>>valorProp;
+                //modificarValorProp(idZona, nomeProp, valorProp);
+            }else{
+                cout << "[ERRO] Sintaxe invalida: pmod <ID zona> <nome> <valor>" << endl;
+            }
+        } else if (command == "cnovo") {
+            if (arguments.size()==4){
+                int idZona;
+                string novoComponente;
+                int tipoComand;
+                istringstream (arguments[1])>>idZona;
+                istringstream (arguments[2])>>novoComponente;
+                istringstream (arguments[3])>>tipoComand;
+                //criarComponente(idZona, novoComponete, tipoComand);
+            }else{
+                cout << "[ERRO] Sintaxe invalida: cnovo <ID zona> <s | p | a> <tipo | comando>" << endl;
             }
         } else {
             cout << "[ERRO] Comando Invalido: " << command << endl;
